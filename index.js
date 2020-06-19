051b80256733913162901b5ec1ac2aace7c87c0a
JoseiToAoiTori <acciowand200@hotmail.com>
2020-06-19 21:45:51 +0300

    versions work now

--- index.js
+++ index.js
@@ -2,32 +2,37 @@ const Snoowrap = require('snoowrap');
 const config = require('./config.json');
 const pages = require('./pages.json');
 const r = new Snoowrap(config);
+const fs = require('fs');
 
 const getPageContent = [];
-
-for (const page of pages) {
-	getPageContent.push(r.getSubreddit('animethemes').getWikiPage(page).content_md);
-}
 
 const json = [];
 
 function getThemes (anime, page, mal) {
 	let tableContent = page.split(anime);
 	tableContent.splice(0, 1);
-	tableContent = tableContent.join('');
+	tableContent = tableContent.join(anime);
 	tableContent = tableContent.split('-|:-:|:-:|:-:|:-:|:-:')[1];
-	tableContent = tableContent.split(/\r?\n\r?\n+/)[0];
+	tableContent = tableContent.split(/\r?\n\r?\n\r?/gm)[0];
 	const themes = tableContent.split(/\r?\n/);
 	if (themes[0].length === 0) themes.splice(0, 1);
 	for (const theme of themes) {
 		const splitData = theme.split('|');
-		const opData = splitData[0].match(/((?:OP|ED)[0-9]*) (.*)/gm);
-		if (!opData) continue;
-		const opNum = opData[1];
-		const opName = opData[2];
+		const opData = splitData[0].split(' ');
+		let opNum;
+		let opName;
+		if (/V[0-9]+/g.test(opData[1])) {
+			opNum = `${opData[0]} ${opData[1]}`;
+			opData.splice(0, 2);
+			opName = opData.join(' ');
+		} else {
+			opNum = opData[0];
+			opData.splice(0, 1);
+			opName = opData.join(' ');
+		}
 		let link = splitData[1].match(/https:\/\/.*\)/gm);
 		if (!link) continue;
-		link = link[0].substring(0, link.length - 1);
+		link = link[0].substring(0, link[0].length - 1);
 		json.push({
 			anime,
 			mal,
@@ -36,6 +41,11 @@ function getThemes (anime, page, mal) {
 			opName,
 		});
 	}
+}
+
+// Execution begins here
+for (const page of pages) {
+	getPageContent.push(r.getSubreddit('animethemes').getWikiPage(page).content_md);
 }
 
 Promise.all(getPageContent).then(pageContent => {
@@ -59,5 +69,6 @@ Promise.all(getPageContent).then(pageContent => {
 			getThemes(anime, page, mal);
 		}
 	}
-	console.log(JSON.stringify(json));
+	// eslint-disable-next-line no-sync
+	fs.writeFileSync('./theme-data.json', JSON.stringify(json, null, 1), 'utf-8');
 });
